0ea61d35d7ee3bc24aa103c65527fb68ef447bec
brix <febrix9@gmail.com>
2017-08-09 05:09:24 +0900

    UITextField Extension을 통한 UIDatePicker 삽입

--- Project/PayRoad/TravelEditorViewController.swift
+++ Project/PayRoad/TravelEditorViewController.swift
@@ -11,28 +11,6 @@ import UIKit
 import RealmSwift
 
 class TravelEditorViewController: UIViewController {
-    let pickerView1: UIDatePicker = {
-        let pickerView = UIDatePicker()
-        pickerView.datePickerMode = .date
-        pickerView.locale = Locale(identifier: "ko_KR")
-        return pickerView
-    }()
-    
-    let pickerView2: UIDatePicker = {
-        let pickerView = UIDatePicker()
-        pickerView.datePickerMode = .date
-        pickerView.locale = Locale(identifier: "ko_KR")
-        return pickerView
-    }()
-    
-    let dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "YYYY년 MM월 dd일"
-        dateFormatter.locale = Locale(identifier: "ko_KR")
-        return dateFormatter
-    }()
-    
-    let realm = try! Realm()
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var startDateTextField: UITextField!
@@ -40,43 +18,17 @@ class TravelEditorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let toolbar1 = UIToolbar()
-        let doneButton1 = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(startDatePickerDonePressed))
-        toolbar1.sizeToFit()
-        toolbar1.setItems([doneButton1], animated: false)
-        
-        startDateTextField.inputAccessoryView = toolbar1
-        startDateTextField.inputView = pickerView1
-        
-        let toolbar2 = UIToolbar()
-        let doneButton2 = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(endDatePickerDonePressed))
-        toolbar2.sizeToFit()
-        toolbar2.setItems([doneButton2], animated: false)
-        
-        endDateTextField.inputAccessoryView = toolbar2
-        endDateTextField.inputView = pickerView2
+
+        startDateTextField.inputDatePicker()
+        endDateTextField.inputDatePicker()
     }
     
-    
-    //TODO: DatePicker 코드 중복 문제 해결과제
-    func startDatePickerDonePressed() {
-        let selectedYear = pickerView1.date
-        startDateTextField.text = dateFormatter.string(from: selectedYear)
-        self.view.endEditing(true)
-    }
-    
-    func endDatePickerDonePressed() {
-        let selectedYear = pickerView2.date
-        endDateTextField.text = dateFormatter.string(from: selectedYear)
-        self.view.endEditing(true)
-    }
     @IBAction func saveButtonDidTap(_ sender: Any) {
         let travel = Travel()
         
         travel.name = titleTextField.text!
-        travel.starteDate = pickerView1.date
-        travel.endDate = pickerView2.date
+        travel.starteDate = (startDateTextField.inputView as? UIDatePicker)!.date
+        travel.endDate = (endDateTextField.inputView as? UIDatePicker)!.date
         
         try? realm.write {
             realm.add(travel)
@@ -91,3 +43,5 @@ class TravelEditorViewController: UIViewController {
     }
     
 }
+
+
