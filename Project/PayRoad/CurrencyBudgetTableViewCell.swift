4a5027414e86f2e28b2bb53446bd801ffd348936
nois0720 <dbtjr0720@gmail.com>
2017-08-16 18:12:30 +0900

    refresh icon추가 currency budget 생성/수정 기능 추가 CurrencyBudgetTableViewCell추가

---
+++ Project/PayRoad/CurrencyBudgetTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyBudgetCell.swift
+//  PayRoad
+//
+//  Created by Yoo Seok Kim on 2017. 8. 16..
+//  Copyright © 2017년 REFUEL. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyBudgetTableViewCell: UITableViewCell {
+    @IBOutlet weak var currencyCodeLabel: UILabel!
+    @IBOutlet weak var currencyLocaleLabel: UILabel!
+    @IBOutlet weak var budgetAmountLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+}
