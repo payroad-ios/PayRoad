15a1faad4955108d5a6cc03cd95c7b3dd6ecb141
brix <febrix9@gmail.com>
2017-08-29 21:06:58 +0900

    MultiImagePickerView 내 선택 Cell에 대해 반투명 검정 LayerView 추가

--- Project/PayRoad/MultiImagePickerCollectionViewCell.swift
+++ Project/PayRoad/MultiImagePickerCollectionViewCell.swift
@@ -17,6 +17,13 @@ class MultiImagePickerCollectionViewCell: UICollectionViewCell {
         return imageView
     }()
     
+    let blackLayerView: UIView = {
+        let layerView = UIView()
+        layerView.backgroundColor = UIColor.black
+        layerView.translatesAutoresizingMaskIntoConstraints = false
+        return layerView
+    }()
+    
     let countLabel: UILabel = {
         let label = UILabel()
         label.backgroundColor = ColorStore.mainSkyBlue
@@ -50,26 +57,34 @@ class MultiImagePickerCollectionViewCell: UICollectionViewCell {
     
     func indicateForStatus() {
         layer.borderWidth = isSelected ? 5 : 0
+        blackLayerView.alpha = isSelected ? 0.3 : 0
         countLabel.isHidden = isSelected ? false : true
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         photoImageView.image = nil
+        blackLayerView.alpha = 0
         countLabel.text = nil
-        layer.borderWidth = 0
         countLabel.isHidden = true
+        layer.borderWidth = 0
     }
     
     func setupViews() {
         let margin: CGFloat = 3
         backgroundColor = ColorStore.unselectGray
         addSubview(photoImageView)
+        addSubview(blackLayerView)
         addSubview(countLabel)
         photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         photoImageView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         photoImageView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        blackLayerView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        blackLayerView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        blackLayerView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        blackLayerView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         
         countLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin).isActive = true
         countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -margin).isActive = true
